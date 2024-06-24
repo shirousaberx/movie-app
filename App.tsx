d722a25d461b7956b010922f7428c5a6841cc7c9
shirousaberx <michaelalexander6666@outlook.com>
2024-06-24 21:32:57 +0700

    Added basic navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { StatusBar } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import BottomTabNavigator from './src/navigations/BottomTabNavigation'
 
 export default function App(): JSX.Element {
   return (
     <NavigationContainer>
+      <StatusBar />
       <BottomTabNavigator />
     </NavigationContainer>
   )
